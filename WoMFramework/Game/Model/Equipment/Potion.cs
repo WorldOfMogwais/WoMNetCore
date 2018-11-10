92cf0e360f1e5a6b759d5b6fa7370ae90e56a8f6
darkfriend77 <darkfriend@swissonline.ch>
2018-11-10 13:18:43 +0100

    started on potion

---
+++ WoMFramework/Game/Model/Equipment/Potion.cs
@@ -0,0 +1,28 @@
+﻿using WoMFramework.Game.Enums;
+using WoMFramework.Game.Model.Spells;
+
+namespace WoMFramework.Game.Model
+{
+    public sealed class PotionBuilder
+    {
+        public string Name  { get; set; }
+        public double Cost { get; set; }
+        public double Weight { get; set; }
+        public string Description = string.Empty;
+
+        public Spell Spell { get; set; }
+
+        public Potion Build()
+        {
+            return new Potion(Name, Cost, Weight, Description, Spell);
+        }
+    }
+
+    public class Potion : BaseItem
+    {
+        public Potion(string name, double cost, double weight, string description, Spell spell) : base(name, cost, weight, description)
+        {
+
+        }
+    }
+}
