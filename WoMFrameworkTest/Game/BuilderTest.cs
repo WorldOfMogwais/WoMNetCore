6fb6fac5f6776d3378d9bc194023bd1c86d9b883
darkfriend77 <darkfriend@swissonline.ch>
2018-11-18 14:56:23 +0100

    fixed tests, less monsters, currently shield equiped monsters are skipped

--- WoMFrameworkTest/Game/BuilderTest.cs
+++ WoMFrameworkTest/Game/BuilderTest.cs
@@ -15,7 +15,7 @@ namespace WoMFrameworkTest.Game
         public void BuilderCreationTest()
         {
             var allMonster = Monsters.Instance.AllBuilders().Select(p => p.Build());
-            Assert.Equal(1321, allMonster.Count());
+            Assert.Equal(1183, allMonster.Count());
             var allArmor = Armors.Instance.AllBuilders().Select(p => p.Build());
             Assert.Equal(62, allArmor.Count());
             var allWeapons = Weapons.Instance.AllBuilders() .Select(p => p.Build());
@@ -26,13 +26,13 @@ namespace WoMFrameworkTest.Game
         public void BuilderMonsterTest()
         {
             var allMonster = Monsters.Instance.AllBuilders().Select(p => p.Build());
-            Assert.Equal(42, allMonster.Where(p => (p.EnvironmentTypes.Contains(EnvironmentType.Any)
+            Assert.Equal(37, allMonster.Where(p => (p.EnvironmentTypes.Contains(EnvironmentType.Any)
                                                  || p.EnvironmentTypes.Contains(EnvironmentType.Undergrounds))
                                                 && p.ChallengeRating <= 0.5).Count());
-            Assert.Equal(110, allMonster.Where(p => p.ChallengeRating < 1).Count());
-            Assert.Equal( 90, allMonster.Where(p => p.ChallengeRating == 1).Count());
-            Assert.Equal(132, allMonster.Where(p => p.ChallengeRating == 2).Count());
-            Assert.Equal(126, allMonster.Where(p => p.ChallengeRating == 3).Count());
+            Assert.Equal(101, allMonster.Where(p => p.ChallengeRating < 1).Count());
+            Assert.Equal( 83, allMonster.Where(p => p.ChallengeRating == 1).Count());
+            Assert.Equal(121, allMonster.Where(p => p.ChallengeRating == 2).Count());
+            Assert.Equal(114, allMonster.Where(p => p.ChallengeRating == 3).Count());
             Assert.Equal("Chickcharney", allMonster.Where(p => p.ChallengeRating == 3 && p.NaturalArmor == 0).First().Name);
 
         }
