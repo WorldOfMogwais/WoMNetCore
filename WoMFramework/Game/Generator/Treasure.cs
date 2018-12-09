63643ebab550209ac02145297d3763dec2fc316e
darkfriend77 <darkfriend@swissonline.ch>
2018-12-09 21:55:05 +0100

    start adding treasures

--- WoMFramework/Game/Generator/Treasure.cs
+++ WoMFramework/Game/Generator/Treasure.cs
@@ -1,6 +1,17 @@
-﻿namespace WoMFramework.Game.Generator
+﻿using System.Collections.Generic;
+using WoMFramework.Game.Model;
+
+namespace WoMFramework.Game.Generator
 {
     public class Treasure
     {
+        public int Gold { get; }
+
+        public List<BaseItem> Items { get; }
+
+        public Treasure()
+        {
+            Items = new List<BaseItem>();
+        }
     }
 }
