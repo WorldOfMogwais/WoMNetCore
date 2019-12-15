857a955c96919305081f25078736d37f35372ee5
darkfriend77 <darkfriend@swissonline.ch>
2019-12-15 22:19:58 +0100

    added arrayMap rotation helper

---
+++ WoMFramework/Tool/ArrayMapHelper.cs
@@ -0,0 +1,46 @@
+﻿using GoRogue.MapViews;
+
+namespace WoMFramework.Tool
+{
+    public enum RotateType
+    {
+        DEG90, DEG180, DEG270
+    }
+
+    public static class ArrayMapHelper
+    {
+        public static ArrayMap<T> RotateArrayMap<T>(ArrayMap<T> arrayMap, RotateType rotateType)
+        {
+            ArrayMap<T> newPattern = null;
+            switch (rotateType)
+            {
+                case RotateType.DEG90:
+                    newPattern = new ArrayMap<T>(arrayMap.Height, arrayMap.Width);
+                    foreach(var pos in arrayMap.Positions())
+                    {
+                        newPattern[pos.Y, arrayMap.Width - 1 - pos.X] = arrayMap[pos.X, pos.Y];
+                    }
+                    return newPattern;
+
+                case RotateType.DEG180:
+                    newPattern = new ArrayMap<T>(arrayMap.Width, arrayMap.Height);
+                    foreach (var pos in arrayMap.Positions())
+                    {
+                        newPattern[arrayMap.Width - 1 - pos.Y, arrayMap.Height - 1 - pos.X] = arrayMap[pos.X, pos.Y];
+                    }
+                    return newPattern;
+
+                case RotateType.DEG270:
+                    newPattern = new ArrayMap<T>(arrayMap.Height, arrayMap.Width);
+                    foreach (var pos in arrayMap.Positions())
+                    {
+                        newPattern[arrayMap.Height - 1 - pos.Y, pos.X] = arrayMap[pos.X, pos.Y];
+                    }
+                    return newPattern;
+
+                default:
+                    return newPattern;
+            }
+        }
+    }
+}
---
+++ WoMFrameworkTest/Tool/ArrayMapHelperTest.cs
@@ -0,0 +1,49 @@
+﻿namespace WoMFrameworkTest.Tool
+{
+    using GoRogue.MapViews;
+    using WoMFramework.Tool;
+    using Xunit;
+ 
+    [Collection("SystemInteractionFixture")]
+    public class ArrayMapHelperTest
+    {
+        [Fact]
+        public void RotateArrayMap()
+        {
+            ArrayMap<bool> arrayMap1 = new ArrayMap<bool>(4,4);
+            arrayMap1[0, 0] = true;
+
+            Assert.True(arrayMap1[0, 0]);
+
+            var arrayMap90deg1 = ArrayMapHelper.RotateArrayMap(arrayMap1, RotateType.DEG90);
+
+            Assert.True(arrayMap90deg1[0, 3]);
+
+            var arrayMap180deg1 = ArrayMapHelper.RotateArrayMap(arrayMap1, RotateType.DEG180);
+
+            Assert.True(arrayMap180deg1[3, 3]);
+
+            var arrayMap270deg1 = ArrayMapHelper.RotateArrayMap(arrayMap1, RotateType.DEG270);
+
+            Assert.True(arrayMap270deg1[3, 0]);
+
+
+            ArrayMap<bool> arrayMap2 = new ArrayMap<bool>(2, 3);
+            arrayMap2[0, 0] = true;
+
+            Assert.True(arrayMap2[0, 0]);
+
+            var arrayMap90deg2 = ArrayMapHelper.RotateArrayMap(arrayMap2, RotateType.DEG90);
+
+            Assert.True(arrayMap90deg2[0, 1]);
+
+            var arrayMap180deg2 = ArrayMapHelper.RotateArrayMap(arrayMap2, RotateType.DEG180);
+
+            Assert.True(arrayMap180deg2[1, 2]);
+
+            var arrayMap270deg2 = ArrayMapHelper.RotateArrayMap(arrayMap2, RotateType.DEG270);
+
+            Assert.True(arrayMap270deg2[2, 0]);
+        }
+    }
+}
